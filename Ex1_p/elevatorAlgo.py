245d739c0011f0bc600fdb44883bc46678aa79ef
Wissam111 <riki_kabha@hotmail.com>
2021-11-12 22:23:16 +0200

    ex1

--- Ex1_p/elevatorAlgo.py
+++ Ex1_p/elevatorAlgo.py
@@ -25,57 +25,5 @@ if __name__ == '__main__':
         pass
 
 
-    def alloc_cost(self, call: Call):
-
-        req_Floor = call.dest
-        dir = call.direction
-        if self.direction == 1:
-            if self.currFloor > _reqFloor or _dir == -1:
-                return self._maxFloor - self.currFloor + self._maxFloor - _reqFloor
-            else:
-                return _reqFloor - self.currFloor
-
-        elif self.direction == -1:
-            if self.currFloor < _reqFloor or _dir == 1:
-                return self.currFloor - 1 + _reqFloor - 1
-            else:
-                return self.currFloor - 1 + _reqFloor
-
-        else:
-
-            return abs(self.currFloor - _reqFloor)
 
 
-    def get_distance(self, call: Call):
-        _reqFloor = call.getDest()
-
-        if self.direction == 1:
-            if self.self.currFloor > _reqFloor:
-                return self._maxFloor - self.currFloor + self._maxFloor - _reqFloor
-            else:
-                return _reqFloor - self.currFloor
-        elif self.direction == -1:
-            if self.currFloor > _reqFloor:
-                return self.currFloor - _reqFloor
-            else:
-                return self.currFloor - 1 + _reqFloor - 1
-        else:
-            return abs(self.currFloor - _reqFloor)
-
-
-
-    for call in calls:
-        print(call.time_coming)
-        call.allocated_to = round_robin()
-
-    # for el in building.elevators:
-    #     print('------------')
-    #     print(f'time {el.time_to_finish_calls()}')
-    #     print(el.up_calls)
-    #     print(el.down_calls)
-    #     print('------------')
-
-    with open('./out.csv', 'w', newline='') as f:
-        csv_writer = csv.writer(f)
-        for call in calls:
-            csv_writer.writerow(call.__dict__())
